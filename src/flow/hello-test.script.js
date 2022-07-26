5302376585156222637ec218338a52ae8167c29a
kaka <28356715@qq.com>
2022-07-26 10:56:24 +0800

    删除demo

--- src/flow/hello-test.script.js
+++
@@ -1,7 +0,0 @@
-
-export const HELLO_TEST = `
-import Hello from 0xTEST
-pub fun main(name:String):String{
- return Hello.sayHi(to:name)
-}
-`;
--- src/flow/list-templates.script.js
+++
@@ -1,8 +0,0 @@
-export const LIST_TEMPLATES = `
-  import DappyContract from 0xDappy
-
-  pub fun main(): {UInt32: DappyContract.Template} {
-    let templates = DappyContract.listTemplates()
-    return templates
-  }
-`;
